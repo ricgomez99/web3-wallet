681c05d38e583b7b7c8048be10d500e82fdadb9b
Andres <argomez.restrepo@gmail.com>
2025-11-26 17:41:01 -0500

    add routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { RouterProvider } from 'react-router/dom'
+import { router } from './routes.tsx'
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <App>
+      <RouterProvider router={router} />
+    </App>
+  </StrictMode>
 )
