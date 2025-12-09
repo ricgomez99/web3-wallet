f5882593c587194f890cc1194a18a49904e3758b
Andres <argomez.restrepo@gmail.com>
2025-12-09 17:57:00 -0500

    fixed eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,31 +1,63 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+
+import prettierConfig from 'eslint-config-prettier'
+
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  // 1. Archivos Ignorados
+  globalIgnores(['dist', 'reports']),
+
+  // 2. Configuración Principal para TypeScript/React
   {
     files: ['**/*.{ts,tsx}'],
+    plugins: {
+      react: react,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+
+    // Extiende solo objetos de configuración plana
     extends: [
       js.configs.recommended,
-      tseslint.configs.recommended,
+      ...tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
-      'eslint:recommended',
-      'plugin:react/recommended',
-      'plugin:react/jsx-runtime',
-      'plugin:react-hooks/recommended',
-      'standard',
-      'prettier',
     ],
+
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      sourceType: 'module',
+
+      // Configuración de variables globales
+      globals: {
+        ...globals.browser,
+        ...globals.es2020,
+      },
+
+      // Configurar el parser de TS
+      parser: tseslint.parser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
     },
+
+    // Configuración para el plugin de React
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+
     rules: {
+      // Reglas de React
       'react/react-in-jsx-scope': 'off',
       'react/prop-types': 'off',
       'react-refresh/only-export-components': [
@@ -34,4 +66,6 @@ export default defineConfig([
       ],
     },
   },
+
+  prettierConfig,
 ])
